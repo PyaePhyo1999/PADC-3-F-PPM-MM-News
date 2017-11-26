fba284af4c478d2fd93059fcf852a55ab215a7e9
Pyae Phyo Maung <ppm421999@gmail.com>
2017-11-26 14:35:48 +0630

    Project Initialize -xml layout -package -application -theme and color

---
+++ app/src/main/java/com/pyaephyomaung/padcmyanmar/news/MMNewsApp.java
@@ -0,0 +1,11 @@
+package com.pyaephyomaung.padcmyanmar.news;
+
+import android.app.Application;
+
+/**
+ * Created by V3-575G on 26-11-2017.
+ */
+
+public class MMNewsApp extends Application {
+
+}
